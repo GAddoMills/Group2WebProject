6f82920cbed4ee5e60bb4f375ef567dffa017b36
addomilg <addomilg@coventry.ac.uk>
2023-02-23 11:32:41 +0000

    initial commit

---
+++ src/main/java/com/example/group2webdevelopmentproject/Group2WebDevelopmentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.group2webdevelopmentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group2WebDevelopmentProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group2WebDevelopmentProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/group2webdevelopmentproject/Group2WebDevelopmentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group2webdevelopmentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2WebDevelopmentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
